d8556b6765a2d8cc079caa3afcd1038a17c2f59a
Peter Keri <keri.peter@e-vista.hu>
2019-12-17 15:27:54 +0100

    Routes

--- src/pages/Client/Home.js
+++ src/pages/Client/Home.js
@@ -23,54 +23,50 @@ function Home() {
 
         <Grid.Row>
             <Grid.Col>
-            <BlogCard
-                title="And this isn't my nose. This is a false one."
-                postUrl="#"
-                description="Look, my liege! The Knights Who Say Ni demand a sacrifice! …Are you suggesting that coconuts migr..."
-                avatarImgSrc="https://tabler.github.io/tabler/demo/faces/female/18.jpg"
-                authorName="Rose Bradley"
-                profileHref="/profile.html"
-                date="3 days ago"
-                imgSrc="https://tabler.github.io/tabler/demo/photos/david-klaasen-54203-500.jpg"
-                imgAlt="Penguin"
-                iconName="heart"
-                iconHref="#"
-                />
+                <BlogCard
+                    title="And this isn't my nose. This is a false one."
+                    postUrl="#"
+                    description="Look, my liege! The Knights Who Say Ni demand a sacrifice! …Are you suggesting that coconuts migr..."
+                    avatarImgSrc="https://tabler.github.io/tabler/demo/faces/female/18.jpg"
+                    authorName="Rose Bradley"
+                    profileHref="/profile.html"
+                    date="3 days ago"
+                    imgSrc="https://tabler.github.io/tabler/demo/photos/david-klaasen-54203-500.jpg"
+                    imgAlt="Penguin"
+                    iconName="heart"
+                    iconHref="#"
+                    />
             </Grid.Col>
             <Grid.Col>
-            <BlogCard
-                title="And this isn't my nose. This is a false one."
-                postUrl="#"
-                description="Look, my liege! The Knights Who Say Ni demand a sacrifice! …Are you suggesting that coconuts migr..."
-                avatarImgSrc="https://tabler.github.io/tabler/demo/faces/female/18.jpg"
-                authorName="Rose Bradley"
-                profileHref="/profile.html"
-                date="3 days ago"
-                imgSrc="https://tabler.github.io/tabler/demo/photos/david-klaasen-54203-500.jpg"
-                imgAlt="Penguin"
-                iconName="heart"
-                iconHref="#"
-                />
+                <BlogCard
+                    title="And this isn't my nose. This is a false one."
+                    postUrl="#"
+                    description="Look, my liege! The Knights Who Say Ni demand a sacrifice! …Are you suggesting that coconuts migr..."
+                    avatarImgSrc="https://tabler.github.io/tabler/demo/faces/female/18.jpg"
+                    authorName="Rose Bradley"
+                    profileHref="/profile.html"
+                    date="3 days ago"
+                    imgSrc="https://tabler.github.io/tabler/demo/photos/david-klaasen-54203-500.jpg"
+                    imgAlt="Penguin"
+                    iconName="heart"
+                    iconHref="#"
+                    />
             </Grid.Col>
         </Grid.Row>
 
-        
-
-        
-
         <Grid.Row cards deck>
-        <Grid.Col md={4}>
-            <Card body="Short content" />
-        </Grid.Col>
-        <Grid.Col md={4}>
-            <Card
-            body={`Extra long content of card. Lorem ipsum dolor sit amet,
-                consetetur sadipscing elitr`}
-            />
-        </Grid.Col>
-        <Grid.Col md={4}>
-            <Card body="Short content" />
-        </Grid.Col>
+            <Grid.Col md={4}>
+                <Card body="Short content" />
+            </Grid.Col>
+            <Grid.Col md={4}>
+                <Card
+                body={`Extra long content of card. Lorem ipsum dolor sit amet,
+                    consetetur sadipscing elitr`}
+                />
+            </Grid.Col>
+            <Grid.Col md={4}>
+                <Card body="Short content" />
+            </Grid.Col>
         </Grid.Row>
 
         <Card
